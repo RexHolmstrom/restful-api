52de9c91dd30910070c582faf720ebd2a377e223
rexholmstrom <rexholmstrom96@gmail.com>
2020-08-30 21:46:59 +0200

    json update v1

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
+const e = require("express");
 
 router.get("/", (req, res) => {
   res.send("Posts page");
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body);
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+  post
+    .save()
+    .exec()
+    .then((data) => {
+      res.json(data);
+    });
 });
 
 module.exports = router;
